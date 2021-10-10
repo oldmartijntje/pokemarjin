83f2eaa562b38863577212c8d9d3e02e83054f35
oldmartijntje <oldmartijntje@gmail.com>
2021-10-10 21:59:36 +0200

    commit3

--- pokemane.py
+++ pokemane.py
@@ -1,9 +1,21 @@
+import time
+import os
+import os.path
+import random
+import datetime
+
 def menu():
     loop1 = True
     while loop1 == True:
-        input1 = input("\nChoose what to do (by typing the right number)\n1.Start Game\n2.Options\n3.Info")
+        input1 = input("\nChoose what to do (by typing the right number)\n1.Start Game\n2.Options\n3.Info\n")
         if input1 != "1" and input1 != "2" and input1 != "3":
             print("sorry, that is not an option")
+        elif input1 == "1":
+            loop1 = False
+def saving(pokemons,team):
+    saveFile = open("yourSaveFile.txt", "a+")
+    saveFile.truncate(0)
+    saveFile.seek(0)
 class Pokemon:
       def __init__(self, name,hp,defense,speed,type,possibleMoves,baseHp,baseDefence,baseSpeed):
         self.name = name
@@ -17,15 +29,28 @@ class Pokemon:
         self.baseSpeed = baseSpeed
 print("Loading...\n")
 if True:
-    p1 = Pokemon("Magikarp",2,1,1,'Water',['m0000','m0001'],10,10,10)
+    p1 = Pokemon("Magikarp",2,1,1,['Water'],['m0','m1'],10,10,10)
+    p2 = Pokemon("Stannina",1,2,1,['Fire'],['m0','m1'],10,10,10)
 print(p1.type)
+
 moveNames = {
     #[name,baseattack,accuracy,special]
-    'm0000' : ['Splash',0,100,0],
-    'm0001' : ['SelfHate',0,100,1]
+    'm0' : ['Splash',0,100,0],
+    'm1' : ['SelfHate',0,100,1]
 }
+
+playtime = 0
+if os.path.isfile("yourSaveFile.txt"):
+    saveFile = open("yourSaveFile.txt", "a")
+    saveFile.read().split(';')
+else:
+    saveFile = open("yourSaveFile.txt", "x")
+saveFile.close()
+
+print(moveNames["m0"])
 lista = [p1,'p2']
 print(vars(lista[0])['name'])
+
 #Start of game
 print("Welkom to PokeMarjin, my own spin-off pokemon game fully made with python")
 menu()
