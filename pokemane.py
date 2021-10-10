37d1036d84236e28d935a98c097196db950a0ee9
oldmartijntje <oldmartijntje@gmail.com>
2021-10-10 22:44:16 +0200

    commit 4, goodnight

--- pokemane.py
+++ pokemane.py
@@ -12,12 +12,14 @@ def menu():
             print("sorry, that is not an option")
         elif input1 == "1":
             loop1 = False
-def saving(pokemons,team):
+def saving(pokemons, team, gameData):
+    gameData[0] +=1
     saveFile = open("yourSaveFile.txt", "a+")
     saveFile.truncate(0)
     saveFile.seek(0)
+    saveFile.write(pokemons, "/", team, "/", gameData)
 class Pokemon:
-      def __init__(self, name,hp,defense,speed,type,possibleMoves,baseHp,baseDefence,baseSpeed):
+      def __init__(self, name, hp, defense, speed, type, possibleMoves, baseHp, baseDefence, baseSpeed):
         self.name = name
         self.hppl = hp
         self.speedpl = speed
@@ -29,27 +31,42 @@ class Pokemon:
         self.baseSpeed = baseSpeed
 print("Loading...\n")
 if True:
-    p1 = Pokemon("Magikarp",2,1,1,['Water'],['m0','m1'],10,10,10)
-    p2 = Pokemon("Stannina",1,2,1,['Fire'],['m0','m1'],10,10,10)
-print(p1.type)
+    p1 = Pokemon("Magikarp",'3','1','1',['Water'],['m1','m2'],'10','10','10')
+    p2 = Pokemon("Stannina",'1','3','1',['Fire'],['m1','m2','m5','m6','m8','m9'],'10','10','10')
+    p3 = Pokemon("Thomas",'2','2','1',['Dark'],['m1','m3','m4','m6','m7'],'10','10','10')
+    p4 = Pokemon("Eevee",'1','2','2',['Normal'],['m0','m2','m4','m7'],'10','10','10')
+#print(p1.type)
 
 moveNames = {
     #[name,baseattack,accuracy,special]
-    'm0' : ['Splash',0,100,0],
-    'm1' : ['SelfHate',0,100,1]
-}
+    'm1' : ['Splash',0,100,0],
+    'm2' : ['SelfHate',0,100,1],#ur speed goes to 0, but ur defense +1
+    'm3' : ['Tackle',3,100,0],
+    'm4' : ['Ember',2,100,0],
+    'm5' : ['Neko power',0,100,2],#enemy falls in love for some rounds
+    'm6' : ['Hacking the mainframe',0,100,3],
+    'm7' : ['Watergun',2,100,0],
+    'm8' : ['Free Premium',0,100,4],#lowers their attacks, defence and speed
+    'm9' : ['Neko Energy',0,100,5],#your speed and attack increases
 
-playtime = 0
+}
+#Stannina",1,2,1,['Fire','none'],['m0','m1','none','none'],10,10,10,level; = 10
+activePokemon = 0
+gameData =[0]
 if os.path.isfile("yourSaveFile.txt"):
     saveFile = open("yourSaveFile.txt", "a")
-    saveFile.read().split(';')
+    pokemonsWrapped, teamWrapped, gameDataWrapped = saveFile.read().split('/')
+    pokemons = pokemonsWrapped.spit(',')
+    team = teamWrapped.spit(',')
+    gameData = gameDataWrapped.spit(',')
+    gameData[0] = int(gameData[0])
 else:
     saveFile = open("yourSaveFile.txt", "x")
 saveFile.close()
 
-print(moveNames["m0"])
-lista = [p1,'p2']
-print(vars(lista[0])['name'])
+#print(moveNames["m0"])
+#lista = [p1,'p2']
+#print(vars(lista[0])['name'])
 
 #Start of game
 print("Welkom to PokeMarjin, my own spin-off pokemon game fully made with python")
