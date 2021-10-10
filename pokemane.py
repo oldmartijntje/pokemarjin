a9e8f895a2de44f18ddb7169847e4c63e21b4146
oldmartijntje <oldmartijntje@gmail.com>
2021-10-10 19:04:55 +0200

    update3

--- pokemane.py
+++ pokemane.py
@@ -1,3 +1,9 @@
+def menu():
+    loop1 = True
+    while loop1 == True:
+        input1 = input("\nChoose what to do (by typing the right number)\n1.Start Game\n2.Options\n3.Info")
+        if input1 != "1" and input1 != "2" and input1 != "3":
+            print("sorry, that is not an option")
 class Pokemon:
       def __init__(self, name,hp,defense,speed,type,possibleMoves,baseHp,baseDefence,baseSpeed):
         self.name = name
@@ -9,9 +15,9 @@ class Pokemon:
         self.basehp = baseHp
         self.baseDefence = baseDefence
         self.baseSpeed = baseSpeed
-
+print("Loading...\n")
 if True:
-    p1 = Pokemon("Magikarp", 2,1,1,'Water',['m0000','m0001'],10,10,10)
+    p1 = Pokemon("Magikarp",2,1,1,'Water',['m0000','m0001'],10,10,10)
 print(p1.type)
 moveNames = {
     #[name,baseattack,accuracy,special]
@@ -20,3 +26,6 @@ moveNames = {
 }
 lista = [p1,'p2']
 print(vars(lista[0])['name'])
+#Start of game
+print("Welkom to PokeMarjin, my own spin-off pokemon game fully made with python")
+menu()
